dc8ae7bf6c4007622632313c3a1a3acde0cd2928
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-01-29 00:12:29 +0530

    Written Header(navbar) HTML only

---
+++ src/App.js
@@ -0,0 +1,16 @@
+ 
+import './App.css';
+import Header from './Components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Programmer , let's clone Amazon Store :🚀   </h1>
+      <Header/>
+      {/* Home*/ }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCx5SwBlTJlPxPz1e8bVnkZgxZFGQZ2W9k",
+    authDomain: "clone-bacad.firebaseapp.com",
+    projectId: "clone-bacad",
+    storageBucket: "clone-bacad.appspot.com",
+    messagingSenderId: "597402139458",
+    appId: "1:597402139458:web:7d9d7901e921f9b8f4a6db",
+    measurementId: "G-77RZ9E6V1V"
+  };
