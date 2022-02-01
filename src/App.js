8f4d99aa9ab2470e2e8e109aa96b0af7d375e103
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-02-01 09:43:02 +0530

    completed header css and added material icon package

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from './Components/Header';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Programmer , let's clone Amazon Store :🚀   </h1>
       <Header/>
       {/* Home*/ }
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Header.css' 
-import logo from './Amazon-logo-light.png'
+import logo from './Amazon-logo-dark.png'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
   return <div className="header">
-        <img src={logo} alt=""/>
+        <img className = "header__logo"src={logo} alt=""/>
         <div className="header__search">
-            <input type="text" className="header__searchInPut" />
-            {/* logo */}
+            <input type="text" className="header__searchInput" />
+            <SearchIcon className='header__searchIcon'/>
         </div>
         <div className="header__nav">
              <div className="header__option">
@@ -21,6 +23,12 @@ function Header() {
              <div className="header__option">
                 <span className="header__optionLineOne">Your</span>
                 <span className="header__optionLineTwo">Prime</span>
+             </div>
+             <div className="header__optionBasket">
+                  <ShoppingCartIcon/>
+                  <span className="header__optionLineTwo header__basketCount">
+                     0
+                  </span>
              </div>
         </div>
   </div>;
