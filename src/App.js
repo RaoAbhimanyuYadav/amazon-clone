e9b68fc6a4408a5562d63f528e4f477075c59c90
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-02-20 17:04:57 +0530

    user name on check out page

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ function App() {
           user: null,
         });
       }
-      console.log("check" + auth.currentUser.email);
     });
   }, []);
   return (
--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -4,13 +4,17 @@ import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal.js";
 
+import { auth } from "../firebase";
+
 export default function Checkout() {
-  const [{ basket }] = useStateValue();
+  const [{ basket, user }] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img src="https://www.helium10.com/app/uploads/2018/12/Banner-Amazon-Advertising-Marketing-Guide-01.png" alt="" className="checkout__ad" />
         <div>
+          <h3>Hello, {user ? user.email : "Guest"}</h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct id={item.id} title={item.title} image={item.image} price={item.price} rating={item.rating} />
