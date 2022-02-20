e28f3d435cd4f867e5fb8a68bf6e8098263ce7b6
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-02-20 16:48:19 +0530

    user authenticaton done using firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCx5SwBlTJlPxPz1e8bVnkZgxZFGQZ2W9k",
-    authDomain: "clone-bacad.firebaseapp.com",
-    projectId: "clone-bacad",
-    storageBucket: "clone-bacad.appspot.com",
-    messagingSenderId: "597402139458",
-    appId: "1:597402139458:web:7d9d7901e921f9b8f4a6db",
-    measurementId: "G-77RZ9E6V1V"
-  };
+  apiKey: "AIzaSyCx5SwBlTJlPxPz1e8bVnkZgxZFGQZ2W9k",
+  authDomain: "clone-bacad.firebaseapp.com",
+  projectId: "clone-bacad",
+  storageBucket: "clone-bacad.appspot.com",
+  messagingSenderId: "597402139458",
+  appId: "1:597402139458:web:7d9d7901e921f9b8f4a6db",
+  measurementId: "G-77RZ9E6V1V",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
